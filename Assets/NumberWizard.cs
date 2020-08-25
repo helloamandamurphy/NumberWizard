29d8d41f68618057db2d7bcefe1ade1cd32d4e11
helloamandamurphy <helloamandamurphy@gmail.com>
2020-08-25 13:33:45 -0700

    Update user prompts; add more logic to guess

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -12,20 +12,25 @@ public class NumberWizard : MonoBehaviour
     void Start() {
         Debug.Log("Welcome to Number Wizard!");
         Debug.Log("Pick a number between " + min + "-" + max + ", but don't tell me what it is...");
+        Debug.Log("Is your number higher or lower than " + guess + "?");
+        Debug.Log("Push the Up Arrow Key for higher, or push the Down Arrow Key for lower; push the Enter key if the number is correct.");
     }
 
     // Update is called once per frame
     void Update() {
      if (Input.GetKeyDown(KeyCode.UpArrow)){
          min = guess;
-         Debug.Log(guess);
+         guess = (max + min) / 2;
+         Debug.Log("Is it higher or lower than " + guess + "?");
      }
      else if (Input.GetKeyDown(KeyCode.DownArrow)){
          max = guess;
-         Debug.Log(guess);
+         guess = (max + min) / 2;
+         Debug.Log("Is it higher or lower than " + guess + "?");
+
      }
      else if (Input.GetKeyDown(KeyCode.Return)){
-         Debug.Log("Return key was pressed.");
+         Debug.Log("I am a genius.");
      }
     }
 }
