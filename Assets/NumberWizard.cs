5867d299a59dc7f157f38fe541a2b6b7ce6d27e9
helloamandamurphy <helloamandamurphy@gmail.com>
2020-08-25 10:50:22 -0700

    Complete variable lesson

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -7,7 +7,11 @@ public class NumberWizard : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        int min = 1;
+        int max = 1000;
+
+        Debug.Log("Welcome to Number Wizard!");
+        Debug.Log("Pick a number between " + min + "-" + max + ", but don't tell me what it is...");
     }
 
     // Update is called once per frame
